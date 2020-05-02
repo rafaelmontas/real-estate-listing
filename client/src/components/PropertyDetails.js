fada3cbe3d332381c7ce5c3d41078e37c9de757e
Rafael Montas <rafaelmontas1@gmail.com>
2020-05-02 19:58:31 -0400

    Add route to find property by id and Add fetch request on client side

--- client/src/components/PropertyDetails.js
+++ client/src/components/PropertyDetails.js
@@ -3,12 +3,31 @@ import PropTypes from 'prop-types';
 import './PropertyDetails.css';
 
 class PropertyDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      property: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch(`/properties/${this.props.match.params.id}`)
+          .then(res => res.json())
+          .then(property => {
+            console.log(property)
+            this.setState({ property });
+          });
+  }
+
+
+
   render() {
     return (
       <div>
         <div>
           <h1>hi {this.props.match.params.id}</h1>
           <button onClick={() => this.props.history.goBack()}>Go Back!</button>
+          <h3>{this.state.property.price}</h3>
         </div>
       </div>
     )
--- routes/properties.js
+++ routes/properties.js
@@ -3,8 +3,10 @@ const propertiesRouter = express.Router();
 const db =  require('../models');
 const Property = db.property;
 
-const properties = require("../data");
 
+// @route GET /properties/
+// @desc Get All Properties
+// @access Public
 propertiesRouter.get("/", (req, res) => {
   Property.findAll().then(properties => {
     res.status(200).send(properties);
@@ -12,6 +14,20 @@ propertiesRouter.get("/", (req, res) => {
     console.log(err);
     res.sendStatus(500);
   });
+})
+
+// @route GET /properties/:id
+// @desc Get One Property
+// @access Public
+propertiesRouter.get("/:id", (req, res) => {
+  Property.findByPk(req.params.id)
+            .then(property => {
+              res.status(200).send(property)
+            })
+            .catch(err => {
+              console.log(err);
+              res.sendStatus(500);
+            });
 })
 
 
