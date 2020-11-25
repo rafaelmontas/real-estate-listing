53c150dd1f3e4f627b12d1b9219fe2b6daa880ba
Rafael Montas <rafael.montas@hauzzy.com>
2020-11-25 13:18:39 -0400

    WEB-3 Finish landing page sections structure

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -3,6 +3,8 @@ import './LandingPage.css';
 import logo from '../../demo_img/brand-logo-vf.svg';
 import rightMap from '../../demo_img/right-img-2.png';
 import connectionImg from '../../demo_img/contact-form.png'
+import macbookMockup from '../../demo_img/macBook-iphone.png'
+import likeCard from '../../demo_img/like-card.png'
 
 class LandingPage extends React.Component {
   render() {
@@ -38,6 +40,26 @@ class LandingPage extends React.Component {
               <img src={connectionImg}/>
             </div>
             <div className="connection-text">
+              <h1>Deja que los clientes se acerquen</h1>
+              <p>Posicionate frente a clientes potenciales que están buscando propiedades
+                 y gestiona con las herramientas que necesitas para responder de manera rapida, efectiva y ampliar tu red de clientes</p>
+            </div>
+          </section>
+          <section className="like-card-section section">
+            <div className="left-info">
+              <h1>Deja que los clientes se acerquen</h1>
+              <p>Posicionate frente a clientes potenciales que están buscando propiedades
+                 y gestiona con las herramientas que necesitas para responder de manera rapida, efectiva y ampliar tu red de clientes</p>
+            </div>
+            <div className="like-card">
+              <img src={likeCard}/>
+            </div>
+          </section>
+          <section className="mockup-section section">
+            <div className="mockup-side">
+              <img src={macbookMockup}/>
+            </div>
+            <div className="right-info">
               <h1>Deja que los clientes se acerquen</h1>
               <p>Posicionate frente a clientes potenciales que están buscando propiedades
                  y gestiona con las herramientas que necesitas para responder de manera rapida, efectiva y ampliar tu red de clientes</p>
