e2061afc6a9c8242f74d4d8249309df7a2b93f4a
Rafael Montas <rafael.montas@hauzzy.com>
2020-12-21 16:13:43 -0400

    WEB-58 Add Agent section based on subdomain

---
+++ client/src/components/AgentsApp/AgentsApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class AgentsApp extends React.Component {
+  render() {
+    return <h1>Agents Dashboard</h1>
+  }
+}
+
+export default AgentsApp;
--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import AgentsApp from './components/AgentsApp/AgentsApp'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const parsedData = window.location.host.split(".");
+const subDomain = parsedData[0];
+
+if(parsedData.length >= 3 && subDomain !== 'www') {
+  console.log(parsedData,parsedData.includes('agent'))
+  if(subDomain === 'agent') {
+    ReactDOM.render(
+      <React.StrictMode>
+        <AgentsApp />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );  
+  }
+} else {
+  console.log(window.location.host)
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
