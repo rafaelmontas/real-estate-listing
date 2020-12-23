b981c0cdf1ed0623c65f3f2b376723e1d05daaf6
Rafael Montas <rafael.montas@hauzzy.com>
2020-12-23 18:49:13 -0400

    WEB-61 Add agent dashboard navbar

---
+++ client/src/components/AgentsApp/AgentDashboard.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import AgentNavbar from './AgentNavbar';
+
+class AgentDashboard extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sideDrawerOpen: false
+    }
+    this.handleSideDrawerToggleClick = this.handleSideDrawerToggleClick.bind(this)
+  }
+
+  handleSideDrawerToggleClick() {
+    this.setState((prevState) => {
+      return {sideDrawerOpen: !prevState.sideDrawerOpen};
+    });
+  }
+
+  render() {
+    return (
+      <div className="agent-dashboard-container">
+        <AgentNavbar 
+          onSideDrawerToggleClick={this.handleSideDrawerToggleClick}
+        />
+      </div>
+    )
+  }
+}
+
+export default AgentDashboard;
--- client/src/components/AgentsApp/AgentsApp.js
+++ client/src/components/AgentsApp/AgentsApp.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import AgentDashboard from './AgentDashboard';
+import AgentLogin from './AgentLogin'
+import './AgentsApp.css'
 
 class AgentsApp extends React.Component {
   render() {
-    return <h1>Agents Dashboard</h1>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard" exact component={AgentDashboard}/>
+          {/* <Route path="/agent/login" exact component={AgentLogin}/> */}
+          {/* <Route path="/solutions" exact component={AgentLogin}/> */}
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
 
