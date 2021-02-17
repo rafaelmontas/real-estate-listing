5050f1442609736419e8d75c308455e52e78047c
Rafael Montas <rafael.montas@hauzzy.com>
2021-02-16 20:00:48 -0400

    WEB-92 add basic info step with Google Places API

--- client/src/components/AgentsApp/NewListing/BasicInfo.js
+++ client/src/components/AgentsApp/NewListing/BasicInfo.js
@@ -1,13 +1,47 @@
 import React from 'react'
+import PlacesAutocomplete, {geocodeByAddress, getLatLng} from 'react-places-autocomplete'
 
 const listingTypes = ['apartment', 'house', 'villa', 'penthouse', 'comercial', 'industrial']
 
 class BasicInfo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className="basic-info-container">
         <h3>Información basica sobre la propiedad</h3>
         <p>Si tienes alguna duda o inconveniente, solo tienes que escribirnos por el chat.</p>
+        <div className="property-address listing-container">
+          <h3>Ubicación de la Propiedad</h3>
+          <span>Especificar dirección con número (#)</span>
+          <PlacesAutocomplete 
+            value={this.props.propertyAddress}
+            onChange={this.props.handleAddressChange}
+            onSelect={this.props.handleSelect}
+            searchOptions={{componentRestrictions: { country: "do" }, types: ['address'], fields: ['address_components']}}>
+              {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                <div>
+                  <input {...getInputProps({
+                    placeholder: 'Dirección de la propiedad ...',
+                    className: 'address-search-input'
+                    })}/>
+                  <div className="autocomplete-dropdown-container">
+                    {loading && <div>Cargando...</div>}
+                    {suggestions.map(suggestion => {
+                      const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item'
+                      return (
+                        <div {...getSuggestionItemProps(suggestion, {className})} key={suggestion.placeId}>
+                          {suggestion.description}
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>
+              )}                                
+          </PlacesAutocomplete>
+        </div>
         <div className="property-type listing-container">
           <h3>Tipo de Propiedad</h3>
           <div className="property-type-options">
@@ -28,13 +62,26 @@ class BasicInfo extends React.Component {
                   valueLabel = 'Naves'
                 }
                 return (
-                  <label key={value}className={this.props.propertyType === value ? "type-option selected" : "type-option"}>
+                  <label key={value} className={this.props.propertyType === value ? "type-option selected" : "type-option"}>
                     {valueLabel}
                     <input type="radio" name="propertyType" value={value} onChange={this.props.handleChange('propertyType')}/>
                   </label>
                 )
               })}
             </fieldset>
+          </div>
+          <div className="listing-type-container">
+            <h3>Tipo de Publicación</h3>
+            <div className="listing-type-options">
+              <label className={this.props.listingType === 'sale' ? "type-option selected" : "type-option"}>
+                Venta
+                <input type="radio" name="listingType" value="sale" onChange={this.props.handleChange('listingType')}/>
+              </label>
+              <label className={this.props.listingType === 'rent' ? "type-option selected" : "type-option"}>
+                Alquiler
+                <input type="radio" name="listingType" value="rent" onChange={this.props.handleChange('listingType')}/>
+              </label>
+            </div>
           </div>
         </div>
       </div>
--- client/src/components/AgentsApp/NewListing/ListingForm.js
+++ client/src/components/AgentsApp/NewListing/ListingForm.js
@@ -4,7 +4,9 @@ import Stepper from '@material-ui/core/Stepper'
 import Step from '@material-ui/core/Step'
 import StepLabel from '@material-ui/core/StepLabel'
 import Button from '@material-ui/core/Button'
+import CircularProgressSpinner from '../../CircularProgressSpinner'
 import BasicInfo from './BasicInfo'
+import {geocodeByAddress, getLatLng} from 'react-places-autocomplete'
 import './ListingForm.css'
 
 
@@ -12,14 +14,35 @@ class ListingForm extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      isLoading: true,
       activeStep: 0,
+      // Step 0
       propertyAddress: '',
+      lat: null,
+      lng: null,
       propertyType: '',
-      email: 'ddef'
+      listingType: ''
     }
     this.nextStep = this.nextStep.bind(this)
     this.prevStep = this.prevStep.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleAddressChange = this.handleAddressChange.bind(this)
+    this.handleAddressSelect = this.handleAddressSelect.bind(this)
+  }
+
+  componentDidMount() {
+    if(!window.google) {
+      insertScript()
+      this.timer = setTimeout(() => {
+        this.setState({isLoading: false})
+        console.log('places api mounted')
+      }, 2000)
+    } else {
+      this.timer = setTimeout(() => {
+        this.setState({isLoading: false})
+        console.log('places api already mounted')
+      }, 1000)
+    }
   }
 
   // Proceed to next stap
@@ -37,14 +60,26 @@ class ListingForm extends React.Component {
     console.log([input], e.target.value)
     this.setState({[input]: e.target.value})
   }
+  handleAddressChange = address => {
+    this.setState({propertyAddress: address})
+  }
+  handleAddressSelect = async value => {
+    const results = await geocodeByAddress(value)
+    const latLng = await getLatLng(results[0])
+    this.setState({propertyAddress: value,lat: latLng.lat, lng: latLng.lng})
+    console.log(value, latLng)
+  }
 
   getStepContent() {
     switch(this.state.activeStep) {
       case 0:
         return <BasicInfo
                   handleChange={this.handleChange}
+                  handleAddressChange={this.handleAddressChange}
+                  handleSelect={this.handleAddressSelect}
                   propertyAddress={this.state.propertyAddress}
-                  propertyType={this.state.propertyType}/>
+                  propertyType={this.state.propertyType}
+                  listingType={this.state.listingType}/>
       case 1:
         return <h1>Detalles</h1>
       case 2:
@@ -55,39 +90,54 @@ class ListingForm extends React.Component {
   }
 
   render() {
-    return (
-      <div className="new-listing-container">
-        <Stepper activeStep={this.state.activeStep} alternativeLabel>
-          <Step key='Información basica'>
-            <StepLabel>Información basica</StepLabel>
-          </Step>
-          <Step key='Detalles'>
-            <StepLabel>Detalles</StepLabel>
-          </Step>
-          <Step key='Fotos'>
-            <StepLabel>Fotos</StepLabel>
-          </Step>
-          <Step key='Confirmar'>
-            <StepLabel>Confirmar</StepLabel>
-          </Step>
-        </Stepper>
-        <div className="step-content">
-          {this.getStepContent()}
+    if(this.state.isLoading) {
+      return <CircularProgressSpinner/>
+    } else {
+      return (
+        <div className="new-listing-container">
+          <Stepper activeStep={this.state.activeStep} alternativeLabel>
+            <Step key='Información basica'>
+              <StepLabel>Información basica</StepLabel>
+            </Step>
+            <Step key='Detalles'>
+              <StepLabel>Detalles</StepLabel>
+            </Step>
+            <Step key='Fotos'>
+              <StepLabel>Fotos</StepLabel>
+            </Step>
+            <Step key='Confirmar'>
+              <StepLabel>Confirmar</StepLabel>
+            </Step>
+          </Stepper>
+          <div className="step-content">
+            {this.getStepContent()}
+          </div>
+          <div className="action-buttons">
+            <Button
+              disabled={this.state.activeStep === 0}
+              onClick={this.prevStep}
+              className={'classes.backButton'}>
+              Atras
+            </Button>
+            <Button variant="contained" color="primary" onClick={this.nextStep}>
+              {this.state.activeStep ===  2 ? 'Enviar' : 'Siguiente'}
+            </Button>
+          </div>
         </div>
-        <div className="action-buttons">
-          <Button
-            disabled={this.state.activeStep === 0}
-            onClick={this.prevStep}
-            className={'classes.backButton'}>
-            Atras
-          </Button>
-          <Button variant="contained" color="primary" onClick={this.nextStep}>
-            {this.state.activeStep ===  2 ? 'Enviar' : 'Siguiente'}
-          </Button>
-        </div>
-      </div>
-    )
+      )
+    }
   }
+}
+
+
+function insertScript() {
+  const {head} = document
+  const script = document.createElement('script')
+  script.id = 'newListingTag'
+  script.type = 'text/javascript'
+  script.async = false
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_API_KEY}&libraries=places&language=es`
+  head.appendChild(script)
 }
 
 export default ListingForm;
