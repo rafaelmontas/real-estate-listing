2aad486d603a7936065325347aba32488b68f384
Rafael Montas <rafael.montas@hauzzy.com>
2021-03-03 18:55:20 -0400

    WEB-6 modify database.js file

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-      "username": process.env.RDS_USERNAME,
-      "password": process.env.RDS_PASSWORD,
-      "database": process.env.RDS_DB_NAME,
-      "host": process.env.RDS_HOSTNAME,
-      "port": process.env.RDS_PORT,
+      "username": process.env.RDS_USERNAME || "rafaelmontas",
+      "password": process.env.RDS_PASSWORD || null,
+      "database": process.env.RDS_DB_NAME || "real_estate_listing",
+      "host": process.env.RDS_HOSTNAME || "127.0.0.1",
+      "port": process.env.RDS_PORT || 5432,
       "dialect": "postgres"
   }
 }
