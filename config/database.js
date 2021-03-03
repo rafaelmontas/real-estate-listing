b1ae71361a1b855d7998d5cea5c47646f8b88494
Rafael Montas <rafael.montas@hauzzy.com>
2021-03-03 16:37:11 -0400

    Change db settings 2 ...

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ module.exports = {
       "password": process.env.RDS_PASSWORD,
       "database": process.env.RDS_DB_NAME,
       "host": process.env.RDS_HOSTNAME,
+      "port": process.env.RDS_PORT,
       "dialect": "postgres"
   }
 }
