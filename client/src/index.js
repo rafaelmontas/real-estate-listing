534b54ee64e61c6db557f2740bd1dc350c3d23b1
Rafael Montas <rafael.montas@hauzzy.com>
2021-03-27 15:27:59 -0400

    Remove console.log from production

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,11 @@ import * as serviceWorker from './serviceWorker';
 const parsedData = window.location.host.split(".");
 const subDomain = parsedData[0];
 
+// Remove console.log() from production
+if (process.env.NODE_ENV !== "development") console.log = () => {};
+
 if(parsedData.length >= 3 && subDomain !== 'www') {
-  // console.log(parsedData,parsedData.includes('agent'))
+  console.log(parsedData,parsedData.includes('agent'))
   if(subDomain === 'agent') {
     ReactDOM.render(
       <React.StrictMode>
