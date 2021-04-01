54ab2185a46e3b5395578ca54280aae2153be7f2
Rafael Montas <rafael.montas@hauzzy.com>
2021-04-01 11:51:05 -0400

    WEB-105 Add config for production db

--- config/database.js
+++ config/database.js
@@ -7,5 +7,13 @@ module.exports = {
       "host": process.env.RDS_HOSTNAME || "127.0.0.1",
       "port": process.env.RDS_PORT || 5432,
       "dialect": "postgres"
+  },
+  "production": {
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host": process.env.RDS_HOSTNAME,
+    "port": process.env.RDS_PORT,
+    "dialect": "postgres"
   }
 }
