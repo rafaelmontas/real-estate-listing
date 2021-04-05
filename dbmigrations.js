b256f8f01da82a64d87346a2f8c4773a1504ffd6
Rafael Montas <rafael.montas@hauzzy.com>
2021-04-05 19:23:36 -0400

    WEB-105 modify dbmigrations file...

--- dbmigrations.js
+++ dbmigrations.js
@@ -1,13 +1,21 @@
-const {exec} = require('child_process');
+(async()=> {
+  const { exec } = require('child_process');
 
-await new Promise((resolve, reject) => {
-  const migrate = exec(
-    'sequelize db:migrate',
-    {env: process.env},
-    err => (err ? reject(err): resolve())
-  );
+  await new Promise((resolve, reject) => {
+    const migrate = exec(
+      'sequelize db:migrate',
+      { env: process.env },
+      (err, stdout, stderr) => {
+        resolve();
+      }
+    );
 
-  // Forward stdout+stderr to this process
-  migrate.stdout.pipe(process.stdout);
-  migrate.stderr.pipe(process.stderr);
-});
+    // Listen for the console.log message and kill the process to proceed to the next step in the npm script
+    migrate.stdout.on('data', (data) => {
+      console.log(data);
+      if (data.indexOf('No migrations were executed, database schema was already up to date.') !== -1) {
+        migrate.kill();
+      }
+    });
+  });
+})();
