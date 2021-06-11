a0f8ed1cde193f9a9927f2294d47888ec83f5016
Rafael Montas <rafael.montas@hauzzy.com>
2021-06-11 16:58:45 -0400

    WEB-138 Remove Intercom

--- client/src/components/AgentsApp/Structure.js
+++ client/src/components/AgentsApp/Structure.js
@@ -5,26 +5,11 @@ import AgentListings from './Listings/AgentListings'
 import AgentListingDetails from './Listings/AgentListingDetails'
 import ListingForm from './NewListing/ListingForm'
 import AgentProfile from './Profile/AgentProfile'
-import {agentContext} from './agentContext';
 import { Route } from 'react-router-dom'
-import Intercom from 'react-intercom';
 
 import './AgentsApp.css'
 
 class Structure extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-    this.user = {
-      user_id: this.context.agent.id,
-      email: this.context.agent.email,
-      name: this.context.agent.name
-    }
-  }
-  
-  // componentDidMount() {
-  //   insertScript()
-  // }
-
   render() {
     return (
       <section className="structure-container">
@@ -35,7 +20,6 @@ class Structure extends React.Component {
             </div>
           </div>
           <div className="right-side-structure">
-            <Intercom appID="xq74edvp" { ...this.user } />
             <Route path="/account/dashboard" component={AgentDashboard}/>
             <Route path="/account/listings" exact component={AgentListings}/>
             <Route path="/account/listings/:id" component={AgentListingDetails}/>
@@ -48,20 +32,4 @@ class Structure extends React.Component {
   }
 }
 
-function intercom(app_id, user_id, name, email, created_at) {
-
-}
-
-function insertScript() {
-  const {head} = document
-  const script = document.createElement('script')
-  script.id = 'newListingTag'
-  script.type = 'text/javascript'
-  script.async = true
-  script.src = `https://survey.survicate.com/workspaces/de7bad5c0a5f47614be2f53b36b09535/web_surveys.js`
-  head.appendChild(script)
-}
-
-
-Structure.contextType = agentContext;
 export default Structure
