33168409e55005121435e973f6517ee403208b4f
Rafael Montas <rafael.montas@hauzzy.com>
2021-09-03 18:05:59 -0400

    Modify title

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,10 +14,7 @@ class Home extends React.Component {
       <div>
         <Helmet>
           <title>Hauzzy: Innovadora plataforma inmobiliaria</title>
-          <meta
-            name="description"
-            content="Innovadora plataforma inmobiliaria. Busca propiedades en venta y en alquiler y conecta con profesionales."
-          />
+          <meta name="description" content="Busca propiedades en venta y en alquiler y conecta con profesionales."/>
           <meta property="og:title" content="Hauzzy: Innovadora plataforma inmobiliaria"/>
           <meta property="og:image" content="https://hauzzy-media-assets.s3.us-east-2.amazonaws.com/fb_post_tiny.png"/>
           <meta property="og:description" content="Busca propiedades en venta y en alquiler y conecta con profesionales."/>
