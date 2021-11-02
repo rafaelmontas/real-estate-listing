ae89fd85028c40bd866e135c47a47f81c64fd445
Rafael Montas <rafael.montas@hauzzy.com>
2021-11-01 21:25:51 -0400

    WEB-155 Work on login modal

--- public/script.js
+++ public/script.js
@@ -18,12 +18,8 @@ function showBackDrop() {
     backDrop.remove()
     let sideDrawer = document.getElementById('side-drawer')
     sideDrawer.classList.remove('open')
-    // if (document.getElementById('auth-modal')) {
-    //   document.getElementById('auth-modal').remove()
-    // }
     if (document.getElementById('user-auth-modal')) {
-      let authModal = document.getElementById('user-auth-modal')
-      authModal.classList.remove('show')
+      document.getElementById('user-auth-modal').remove()
     }
   })
   // Insert backdrop after header
@@ -44,19 +40,63 @@ burgerMenu.addEventListener('click', burgerMenuClickHandler)
 
 
 // Handle login/signup buttons
-let authButton = document.getElementById('login-button')
+// let authButton = document.getElementById('login-button')
 let authButtons = document.querySelectorAll('.auth')
-console.log(authButton, authButtons)
+console.log(authButtons)
 function authClickHandler(event) {
   console.log(event.target.id)
   showBackDrop()
   // Show Modal
-  let authModal = document.getElementById('user-auth-modal')
-  authModal.classList.add('show')
+  // let authModal = document.getElementById('user-auth-modal')
+  // authModal.classList.add('show')
   // Create modal
-  // let authModal = document.createElement('div')
-  // authModal.id = 'auth-modal'
-  // document.body.appendChild(authModal)
+  let authModal = document.createElement('div')
+  authModal.id = 'user-auth-modal'
+  let loginForm = `
+  <form class="login-form">
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email" name="email" id="email" placeholder="Dirección de email"/>
+    </div>
+    <div class="form-group">
+      <label for="password">Contraseña</label>
+      <input type="password" name="password" id="password" placeholder="Constraseña"/>
+    </div>
+    <div class="form-group">
+      <button type="submit">Iniciar sesión</button>
+    </div>
+    <div class="forgot-pss">
+      <a href="/forgot-password">Olvidaste tu contraseña?</a>
+    </div>
+  </form>
+  `
+  authModal.innerHTML = `
+  <div class="container">
+    <div class="auth-header">
+      <div class="action-text">
+        <h2>${event.target.id === 'login-button' ? 'Inicia Sesión' : 'Registrate'}</h2>
+        <p>${event.target.id === 'login-button' ? 'Necesitas una cuenta? ' : 'Tienes una cuenta? '}
+          <span>${event.target.id === 'login-button' ? 'Registrate' : 'Inicia Sesión'}</span>
+        </p>
+      </div>
+      <div>
+        <span class="close-button">
+          <i class="far fa-times-circle"></i>
+        </span>
+      </div>
+    </div>
+    <div class="auth-body">
+      ${loginForm}
+    </div>
+  </div>
+  `
+  document.body.appendChild(authModal)
+  let closeButton = document.getElementsByClassName('close-button')[0]
+  closeButton.addEventListener('click', () => {
+    let backDrop = document.getElementById('backdrop')
+    backDrop.remove()
+    authModal.remove()
+  })
 }
 authButtons.forEach((button) => {
   button.addEventListener('click', authClickHandler)
